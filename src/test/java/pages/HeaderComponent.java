8612a13e1100263a18906b6a8c5814c05748a296
Anna Pilyugina <anna1_89@mail.ru>
2026-09-02 09:45:46 +0300

    Steps added to tests

--- src/test/java/pages/HeaderComponent.java
+++ src/test/java/pages/HeaderComponent.java
@@ -1,6 +1,7 @@
 package pages;
 
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Selenide.$;
 
@@ -14,31 +15,37 @@ public class HeaderComponent {
     private final SelenideElement searchButton = $("button[class='button button_size-l header__search-btn']");
 
     //Actions
+    @Step("Нажимаем на кнопку открытия каталога")
     public HeaderComponent clickCatalogButton() {
         catalogButton.click();
         return this;
     }
 
+    @Step("Нажимаем на кеопку авторизации")
     public HeaderComponent clickLoginButton() {
         loginButton.click();
         return this;
     }
 
+    @Step("Нажимаем на кнопку перехода в Избранное")
     public HeaderComponent clickFavoritesButton() {
         favoritesButton.click();
         return this;
     }
 
+    @Step("Нажимаем на кнопку перехода в Корзину")
     public HeaderComponent clickCartButton() {
         cartButton.click();
         return this;
     }
 
+    @Step("Вводим поисковый запрос")
     public HeaderComponent enterSearchProduct(String data) {
         searchField.setValue(data);
         return this;
     }
 
+    @Step("Нажимаем на кнопку поиска")
     public HeaderComponent clickSearchButton() {
         searchButton.click();
         return this;
--- src/test/java/pages/SearchPage.java
+++ src/test/java/pages/SearchPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Condition.text;
@@ -17,12 +18,14 @@ public class SearchPage {
     private final String buttonTitleAddedToFavorites = "Добавлен в избранное";
 
     //Actions
+    @Step("Нажимем на кнопку добавления товара в Корзину")
     public SearchPage clickAddToCartButton(String data) {
         SelenideElement addToCartButton = $x("//a[contains(text(),'" + data + "')]/ancestor::div[contains(@class, 'product-preview__content')]//button[contains(@class, 'button add-cart-counter__btn')]");
         addToCartButton.click();
         return this;
     }
 
+    @Step("Нажимаем на кнопку добавления товара в Избранное")
     public SearchPage clickAddToFavoritesButton(String data) {
         SelenideElement addToFavoritesButton = $x("//a[contains(text(),'" + data + "')]/ancestor::div[contains(@class, 'product-preview__content')]//button[contains(@class, 'favorites_trigger button is-icon favorites-not-added')]");
         addToFavoritesButton.click();
@@ -30,23 +33,27 @@ public class SearchPage {
     }
 
     //Asserts
+    @Step("Проверяем, что товар успешно найден в Каталоге")
     public SearchPage assertSuccessSearchResult(String data) {
         SelenideElement searchResult = $x("//a[contains(text(),'" + data + "')]");
         searchResult.should(exist);
         return this;
     }
 
+    @Step("Проверяем, что товар не должен быть найден в Каталоге")
     public SearchPage assertNotFoundSearchResult() {
         productNotFoundMessage.shouldHave(text(productNotFoundText));
         return this;
     }
 
+    @Step("Проверяем, что товар добавлен в Корзину")
     public SearchPage assertProductAddedToCart(String data) {
         SelenideElement addedToCartButton = $x("//a[contains(text(),'" + data + "')]/ancestor::div[contains(@class, 'product-preview__content')]//span[contains(@class, 'add-cart-counter__detail-text')]");
         addedToCartButton.shouldHave(text(buttonTextAddedToCart));
         return this;
     }
 
+    @Step("Проверяем, что товар добален в Избранное")
     public SearchPage assertProductAddedToFavorites(String data) {
         SelenideElement addedToFavoritesButton = $x("//a[contains(text(),'" + data + "')]/ancestor::div[contains(@class, 'product-preview__content')]//button[contains(@class, 'favorites_trigger button is-icon favorites-added')]");
         String title = addedToFavoritesButton.getAttribute("title");
