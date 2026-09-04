baea4580028d9f1e6343dc901aadf6dad078bf72
Anna Pilyugina <anna1_89@mail.ru>
2026-09-04 16:40:48 +0300

    Убраны лямбда шаги в тестах

--- src/test/java/tests/HeaderComponentTests.java
+++ src/test/java/tests/HeaderComponentTests.java
@@ -4,8 +4,6 @@ import io.qameta.allure.Story;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static io.qameta.allure.Allure.step;
-
 @Story("Работоспособность кнопок головной компоненты")
 public class HeaderComponentTests extends TestBase{
     String productInCatalog = "Медовое мыло \"Тонус\" с маточным молочком";
@@ -15,45 +13,33 @@ public class HeaderComponentTests extends TestBase{
     @Test
     @DisplayName("Проверка раскрытия каталога")
     void catalogPageShouldAppear() {
-        step("Открыть главную страницу", () ->
-                mainPage.openPage());
-        step("Нажать на кнопку раскрытия каталога", () ->
-                headerComponent.clickCatalogButton());
-        step("Проверить раскрытие каталога", () ->
-                catalogPage.assertCatalogIsOpened());
+        mainPage.openPage();
+        headerComponent.clickCatalogButton();
+        catalogPage.assertCatalogIsOpened();
     }
 
     @Test
     @DisplayName("Проверка открытия окна для авторизации")
     void loginPageShouldAppear() {
-        step("Открыть главную страницу", () ->
-                mainPage.openPage());
-        step("Нажать на кнопку авторизации", () ->
-                headerComponent.clickLoginButton());
-        step("Проверить появление окна авторизации", () ->
-                loginPage.assertLoginPageIsOpened());
+        mainPage.openPage();
+        headerComponent.clickLoginButton();
+        loginPage.assertLoginPageIsOpened();
     }
 
     @Test
     @DisplayName("Проверка открытия окна корзины")
     void cartPageShouldAppear() {
-        step("Открыть страницу аптеки", () ->
-                mainPage.openPage());
-        step("Нажать на кнопку корзины", () ->
-                headerComponent.clickCartButton());
-        step("Проверить появление окна корзины", () ->
-                cartPage.assertCartPageIsOpened());
+        mainPage.openPage();
+        headerComponent.clickCartButton();
+        cartPage.assertCartPageIsOpened();
     }
 
     @Test
     @DisplayName("Проверка открытия окна избранное")
     void favoritesPageShouldAppear() {
-        step("Открыть страницу аптеки", () ->
-                mainPage.openPage());
-        step("Нажать на кнопку избранного", () ->
-                headerComponent.clickFavoritesButton());
-        step("Проверить появление окна избранного", () ->
-                favoritesPage.assertFavoritesPageIsOpened());
+        mainPage.openPage();
+        headerComponent.clickFavoritesButton();
+        favoritesPage.assertFavoritesPageIsOpened();
     }
 
     @Story("Работоспособность поисковой строки")
@@ -61,26 +47,18 @@ public class HeaderComponentTests extends TestBase{
     @Test
     @DisplayName("Проверка поиска при наличии товара в Каталоге")
     void productShouldBeFound() {
-        step("Открыть страницу аптеки", () ->
-                mainPage.openPage());
-        step("Ввести запрос на поиск", () ->
-                headerComponent.enterSearchProduct(productInCatalog));
-        step("Нажать на поиск", () ->
-                headerComponent.clickSearchButton());
-        step("Проверить результат поиска", () ->
-                searchPage.assertSuccessSearchResult(productInCatalog));
+        mainPage.openPage();
+        headerComponent.enterSearchProduct(productInCatalog);
+        headerComponent.clickSearchButton();
+        searchPage.assertSuccessSearchResult(productInCatalog);
     }
 
     @Test
     @DisplayName("Проверка поиска при отсутствии товара в Каталоге")
     void productNotShouldBeFound() {
-        step("Открыть страницу аптеки", () ->
-                mainPage.openPage());
-        step("Ввести запрос на поиск", () ->
-                headerComponent.enterSearchProduct(productNotInCatalog));
-        step("Нажать на поиск", () ->
-                headerComponent.clickSearchButton());
-        step("Проверить результат поиска", () ->
-                searchPage.assertNotFoundSearchResult());
+        mainPage.openPage();
+        headerComponent.enterSearchProduct(productNotInCatalog);
+        headerComponent.clickSearchButton();
+        searchPage.assertNotFoundSearchResult();
     }
 }
--- src/test/java/tests/SearchPageResultsTests.java
+++ src/test/java/tests/SearchPageResultsTests.java
@@ -4,8 +4,6 @@ import io.qameta.allure.Story;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static io.qameta.allure.Allure.step;
-
 @Story("Работа с результатами поиска")
 public class SearchPageResultsTests extends TestBase{
     String productInCatalog = "Медовое мыло \"Тонус\" с маточным молочком";
@@ -13,31 +11,21 @@ public class SearchPageResultsTests extends TestBase{
     @Test
     @DisplayName("Проверка добавления найденного товара в Корзину")
     void productShouldBeAddedToCart() {
-        step("Открыть страницу аптеки", () ->
-                mainPage.openPage());
-        step("Ввести запрос на поиск", () ->
-                headerComponent.enterSearchProduct(productInCatalog));
-        step("Нажать на поиск", () ->
-                headerComponent.clickSearchButton());
-        step("Нажать на добавление найденного товара в корзину", () ->
-                searchPage.clickAddToCartButton(productInCatalog));
-        step("Проверить добавление товара в корзину", () ->
-                searchPage.assertProductAddedToCart(productInCatalog));
+        mainPage.openPage();
+        headerComponent.enterSearchProduct(productInCatalog);
+        headerComponent.clickSearchButton();
+        searchPage.clickAddToCartButton(productInCatalog);
+        searchPage.assertProductAddedToCart(productInCatalog);
     }
 
     @Test
     @DisplayName("Проверка добавления найденного товара в Избранное")
     void productShouldBeAddedToFavorites() {
-        step("Открыть страницу аптеки", () ->
-                mainPage.openPage());
-        step("Ввести запрос на поиск", () ->
-                headerComponent.enterSearchProduct(productInCatalog));
-        step("Нажать на поиск", () ->
-                headerComponent.clickSearchButton());
-        step("Нажать на добавление найденного товара в корзину", () ->
-                searchPage.clickAddToFavoritesButton(productInCatalog));
-        step("Проверить добавление товара в корзину", () ->
-                searchPage.assertProductAddedToFavorites(productInCatalog));
+        mainPage.openPage();
+        headerComponent.enterSearchProduct(productInCatalog);
+        headerComponent.clickSearchButton();
+        searchPage.clickAddToFavoritesButton(productInCatalog);
+        searchPage.assertProductAddedToFavorites(productInCatalog);
     }
 
 
